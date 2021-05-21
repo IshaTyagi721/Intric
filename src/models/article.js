13822c91fc65a325e548071be01eb1e0e6a2056c
IshaTyagi721 <ishatyagi721@gmail.com>
2021-05-21 19:11:03 +0530

    comments added

---
+++ src/models/article.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const articleSchema = new mongoose.Schema({
+    title: { 
+        type: String 
+    },
+    title_sub: { 
+        type: String 
+    },
+    Content : {
+        type : String,
+        trim : true,
+        required : true
+    },
+    date: {
+        type: Date, 
+        default: Date.now
+    },
+    Completed : {
+        type : Boolean,
+        default : false
+    },
+    owner : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : 'User'
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+      }]
+})
+
+articleSchema.virtual('url').get(function(){
+    return '/articles/' + this._id
+ })
+
+module.exports = mongoose.model('Article', articleSchema);
