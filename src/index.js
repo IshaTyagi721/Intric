b0b2d6150d4802cd0d630e8338291637163c96d5
Siddhant <40394241+saskee1999@users.noreply.github.com>
2021-05-22 16:18:09 +0530

    Basic Frontend

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Article = require('./models/article') 
-const Comment = require('./models/comment')
+// const Comment = require('./models/comment')
 const userRouter = require('./routers/user')
 const articleRouter = require('./routers/article')
-const commentRouter = require('./routers/comment')
+// const commentRouter = require('./routers/comment')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -13,8 +13,9 @@ const port = process.env.PORT || 3001
 app.use(express.json())
 app.use(userRouter)
 app.use(articleRouter)
-app.use(commentRouter)
+// app.use(commentRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port'+" "+port)
 })
+
