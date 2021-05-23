bf1ba8b17b294622810546450526e0929a0fdfaa
IshaTyagi721 <ishatyagi721@gmail.com>
2021-05-23 21:16:45 +0530

    final

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+//Database url
 mongoose.connect('mongodb://127.0.0.1:27017/intric', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
