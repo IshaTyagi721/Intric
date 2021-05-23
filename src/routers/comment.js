e7402f554b91cd5f4825d4a4d5bca6a531d6748b
IshaTyagi721 <59497933+IshaTyagi721@users.noreply.github.com>
2021-05-23 16:01:29 +0530

    Update comment.js

--- src/routers/comment.js
+++ src/routers/comment.js
@@ -4,31 +4,6 @@ const router = express.Router()
 const auth = require('../middleware/auth')
 const Article = require('../models/article')
 
-// router.post('/articles/:id/comment', async (req, res) => {
-//     // find out which post you are commenting
-//      const id = req.params.id;
-//     // get the comment text and record post id
-//      const comment = new Comment({
-//    //   text: req.body.comment,
-//    //   article: id
-//         ...req.body,
-//         article : id
-//   })
-//     // save comment
-//  await comment.save();
-//     // get this particular post
-//  const postRelated = await Article.findById(id);
-//     // push the comment into the post.comments array
-//  postRelated.comments.push(comment);
-//     // save and redirect...
-//   res.status(201).send(comment)
-//  await postRelated.save(function(err) {
-//  if(err) {console.log(err)}
-//  res.redirect('/')
-//  })
-
-// })
-
 router.post('/articles/:id/comment', async (req, res) => {
    // find out which post you are commenting
     const id = req.params.id;
@@ -53,28 +28,27 @@ res.status(201).send(comment)
 })
 
 })
-
-// router.delete("/comments/:postId/:commentId", async function (req, res) {
-//    try {
-//      const post = await Post.findByIdAndUpdate(
-//        req.params.postId,
-//        {
-//          $pull: { comments: req.params.commentId },
-//        },
-//        { new: true }
-//      );
+router.delete("/comments/:articleId/:commentId", async function (req, res) {
+   try {
+     const article = await Article.findByIdAndUpdate(
+       req.params.articleId,
+       {
+         $pull: { comments: req.params.commentId },
+       },
+       { new: true }
+     );
  
-//      if (!post) {
-//        return res.status(400).send("Post not found");
-//      }
+     if (!article) {
+       return res.status(400).send("article not found");
+     }
  
-//      await Comment.findByIdAndDelete(req.params.commentId);
+     await Comment.findByIdAndDelete(req.params.commentId);
  
-//      res.send("Success");
-//    } catch (err) {
-//      console.log(err);
-//      res.status(500).send("Something went wrong");
-//    }
-//  });
+     res.send("Success");
+   } catch (err) {
+     console.log(err);
+     res.status(500).send("Something went wrong");
+   }
+ });
 
 module.exports = router 
